7200cc9baf9a254f96cb543aa18327a2d670ccba
Alessandra Poblador <ale.poblador@gmail.com>
2014-03-02 23:07:01 -0500

    Updated gemfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,20 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ContactExchange::Application.config.secret_key_base = 'ee7b0993619cd996159e624e5950586241cdee4bfd1aba8237197fa9d05d1ad18eefe6ade14d87f45331c2d1618db5e80ec951ec5b2b847d130a6a7c1547824b'
+
+require 'securerandom'
+
+def secure_token
+	token_file = Rails.root.join('.secret')
+	if File.exist?(token_file)
+		# Use the existing token.
+		File.read(token_file).chomp
+	else
+		#Generate a new token and store it in token_file
+		token = SecureRandom.hex(64)
+		File.write(token_file, token)
+		token
+	end
+end
+
+ContactExchange::Application.config.secret_key_base = secure_token
