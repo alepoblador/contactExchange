1ee60ffc74810a40c66de53470e3dae85337f6c2
Alessandra Poblador <ale.poblador@gmail.com>
2014-03-04 05:11:29 -0500

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
 	validates(:email, presence: true, format: { with: VALID_EMAIL_REGEX},
 		uniqueness: {case_sensitive: false} )
 	has_secure_password
-	validates :password, length: { minimum: 8 }
+	validates :password, length: { minimum: 6 }
 
 end
